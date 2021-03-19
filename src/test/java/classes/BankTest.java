9e6d28ebe12f27efae44e2b4435d1ba28e1e73d0
Kwabena Dwamena <kadwamena@gmail.com>
2021-03-19 15:14:16 +0000

    test

---
+++ src/test/java/classes/BankTest.java
@@ -0,0 +1,22 @@
+package classes;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BankTest {
+
+    @org.junit.jupiter.api.Test
+    void addBranch() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void addCustomers() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void addCustomerTransaction() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void listCustomers() {
+    }
+}
---
+++ src/test/java/classes/CustomerTest.java
@@ -0,0 +1,18 @@
+package classes;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CustomerTest {
+
+    @org.junit.jupiter.api.Test
+    void addTransactions() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getName() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getTransactions() {
+    }
+}
